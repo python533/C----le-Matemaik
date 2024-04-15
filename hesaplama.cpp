671106375a9b73904bbc6c49e96554ff14da3526
python533 <143389231+python533@users.noreply.github.com>
2024-04-15 20:50:17 +0300

    C++ Matematik
    13195 Asal Çarpanları 5 ,7,13 ve 29
    600851475143 sayısının En büyük asal çarpanları hangi sayılardır?

---
+++ hesaplama.cpp
@@ -0,0 +1,29 @@
+#include <ctime>
+using namespace std;
+#include <cmath>
+int main(){
+setlocale(LC_ALL,"Turkish");
+int t0, num=600851475143;
+string check;
+string i;
+int LPF=0;
+t0=time.time();
+list=primeList(int(math.sqrt(num)));
+check=True;
+İ=len(list) -1;
+
+while (check) {
+if (num % list[i] ==0) {
+  
+}
+}
+
+
+
+
+
+
+
+
+
+}
---
+++ problem.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+int main(){
+  setlocale(LC_ALL,"Turkish");
+int p=2;
+int n=2;
+}
+
+
+int main(){
+  int prime[n+1]
+  int prime2[p*2,n+1,p]
+  int primes[]
+
+  for (size_t i = 0; i < prime.length; i++) {
+  while p *p  <= n:
+  if (prime[p]) {
+  for (size_t i = 0; i < prime2.length; i++) {
+    prime[i]= False;
+    p+=1;
+  }
+  while (n <len(prime)) {
+    if (prime[n]) {
+      primes[].append(n);
+      n+=1;
+      LFP=list[i]
+      i -=1;
+      t1=time.time();
+    }
+      std::cout << "Bu kadar Zamanda bu kadar sayı bulundu",t1-t0;
+      std::cout << "LPF",LPF;
+
+  }
+  }
+  }
+}
